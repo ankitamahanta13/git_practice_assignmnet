2b4e9114249fa4a5b97fdd95084fb14fb4aab2e5
adityanarayankhatua <adityanarayankhatua4@gmail.com>
2022-10-25 23:48:07 +0530

    all r ok

--- index.js
+++ index.js
@@ -16,17 +16,4 @@ else
   console.log("not prime");
 }
 
-
-// let str="mom";
-// let new_str="";
-// for(let i=str.length-1;i>=0;i--)
-// {
-//   new_str+=str[i];
-// }
-// if(new_str==str)
-// {
-//   console.log("palindrome");
-// }
-// else{
-//    console.log("not palindrome");
-// }
+//abcdefdghjrgjr
